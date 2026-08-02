7550b9a99c75fd233cafb4af65ace83f9bf6df0e
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-08-02 19:05:35 -0400

    catch if db returns error

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,10 @@ import Link from "next/link";
 
 export default async function Home() {
   const supabase = await createClient();
-  const { data: { user } } = await supabase.auth.getUser();
+  const user = await supabase.auth
+    .getUser()
+    .then(({ data, error }) => (error ? null : data.user))
+    .catch(() => null);
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
