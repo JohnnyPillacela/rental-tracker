a0618060c94335831de25583c3ba0f637e3c3ae4
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-09-05 09:58:02 -0400

    Added spinner when loading

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+// app/loading.tsx
+
+import { Spinner } from "@/components/ui/spinner";
+
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Spinner className="size-8" />
+    </div>
+  );
+}
